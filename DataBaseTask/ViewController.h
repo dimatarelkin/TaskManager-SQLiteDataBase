4b7bb5a52c41f481c31d78fe4a39004de5e6b105
Dzmitry Tarelkin <dzmitry_tarelkin@epbyminw0333.minsk.epam.com>
2018-07-09 10:17:32 +0300

    Initial Commit

---
+++ DataBaseTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DataBaseTask
+//
+//  Created by Dzmitry Tarelkin on 7/9/18.
+//  Copyright © 2018 Dzmitry Tarelkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DataBaseTask/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DataBaseTask
+//
+//  Created by Dzmitry Tarelkin on 7/9/18.
+//  Copyright © 2018 Dzmitry Tarelkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
